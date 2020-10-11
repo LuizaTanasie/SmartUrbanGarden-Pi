2c3209a72278397d2bedd8a9d3370ac386eff24d
Luiza Tanasie <luiza.tanasie@qubiz.com>
2020-10-11 23:26:14 +0300

    Fixes

--- SmartPotData.py
+++ SmartPotData.py
@@ -1,6 +1,12 @@
+import json
+
 class SmartPotData:
     def __init__(self, temperature, humidity, soil_moisture, is_raining):
         self.temperature = temperature
         self.humidity = humidity
         self.soil_moisture = soil_moisture
         self.is_raining = is_raining
+
+    def toJSON(self):
+        return json.dumps(self, default=lambda o: o.__dict__,
+                          sort_keys=True, indent=4)
